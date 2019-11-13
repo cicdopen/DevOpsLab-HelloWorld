2a7573f02cb00fde697086893415e16d7b0ffc55
cicdopen <57717250+cicdopen@users.noreply.github.com>
2019-11-13 16:58:17 -0300

    Devmind commit in 2019-11-13 16:58:16

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Escreva uma Mensagem para o Cabeçalho da Página.")
+        self.assertRegex(result.data.decode(), "Curso DevOps Essentials 4linux - FREE.")
 
 
 if __name__ == "__main__":
